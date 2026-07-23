b109ab1a03bbfb612e656ea2ef1b1c619d3d43cb
michaelcwoodworth <michaelcwoodworth@gmail.com>
2026-07-23 05:19:20 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('[data-carousel]').forEach(carousel => {
-  const track = carousel.querySelector('.track');
-  const left = carousel.querySelector('[data-left]');
-  const right = carousel.querySelector('[data-right]');
-
-  left.onclick = () => track.scrollBy({ left: -340, behavior: 'smooth' });
-  right.onclick = () => track.scrollBy({ left: 340, behavior: 'smooth' });
-});
